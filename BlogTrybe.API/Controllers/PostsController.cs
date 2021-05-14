ac29f1293580d2a51cc68040fa88aaee30ea7e39
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-05-14 11:31:42 -0300

    Teste Trybe

---
+++ BlogTrybe.API/Controllers/PostsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogTrybe.API.Controllers
+{
+    [Route("api/posts")]
+    public class PostsController : ControllerBase
+    {
+        public PostsController()
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpGet("{search}")]
+        public async Task<IActionResult> Get(string search)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ BlogTrybe.API/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogTrybe.API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}/login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
