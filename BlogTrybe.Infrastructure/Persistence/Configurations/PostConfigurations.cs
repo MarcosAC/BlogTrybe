5d1c37c1fee6a68965557f55907caee570a76332
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-05-15 12:50:54 -0300

    Configurations

---
+++ BlogTrybe.Infrastructure/Persistence/Configurations/PostConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using BlogTrybe.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogTrybe.Infrastructure.Persistence.Configurations
+{
+    public class PostConfigurations : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.HasKey(post => post.Id);
+
+            builder.HasOne(post => post.User)
+                .WithMany(user => user.UserPosts)
+                .HasForeignKey(post => post.IdUser)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
