3f7e723c28022dac6363aa7e933441de719ecb48
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-05-15 12:28:13 -0300

    Serviço de autenticação

---
+++ BlogTrybe.Infrastructure/Auth/AuthService.cs
@@ -0,0 +1,18 @@
+﻿using BlogTrybe.Core.Services;
+using System;
+
+namespace BlogTrybe.Infrastructure.Auth
+{
+    public class AuthService : IAuthService
+    {
+        public string ComputeSha256Hash(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateJwtToken(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
