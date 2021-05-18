224b13c4057e547526c2119e4062a9ccc03d296d
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-05-18 18:42:27 -0300

    Implemetação dos metodos no controller

--- BlogTrybe.API/Controllers/PostsController.cs
+++ BlogTrybe.API/Controllers/PostsController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using BlogTrybe.Application.Commands.CreatePost;
+using BlogTrybe.Application.Commands.DeletePost;
+using BlogTrybe.Application.Commands.UpDatePost;
+using BlogTrybe.Application.Queries.GetAllPost;
+using BlogTrybe.Application.Queries.GetPostById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace BlogTrybe.API.Controllers
@@ -9,15 +12,19 @@ namespace BlogTrybe.API.Controllers
     [Route("api/posts")]
     public class PostsController : ControllerBase
     {
-        public PostsController()
-        {
+        private readonly IMediator _mediator;
 
+        public PostsController(IMediator mediator)
+        {
+            _mediator = mediator;
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post()
+        public async Task<IActionResult> Post([FromBody] CreatePostCommand command)
         {
-            return Ok();
+            var id = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetById), new { id = id}, command);
         }
 
         [HttpGet]
@@ -29,24 +36,41 @@ namespace BlogTrybe.API.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            return Ok();
+            var query = new GetPostByIdQuery(id);
+
+            var post = await _mediator.Send(query);
+
+            if (post == null)
+                return NotFound();
+
+            return Ok(post);
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id)
+        public async Task<IActionResult> Put(int id, [FromBody] UpDatePostCommand command)
         {
+            await _mediator.Send(command);
+
             return NoContent();
         }
 
-        [HttpGet("{search}")]
-        public async Task<IActionResult> Get(string search)
+        [HttpGet]
+        public async Task<IActionResult> Get(string query)
         {
-            return Ok();
+            var getAllPostQuery = new GetAllPostsQuery(query);
+
+            var posts = await _mediator.Send(getAllPostQuery);
+
+            return Ok(posts);
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
+            var command = new DeletePostCommand(id);
+
+            await _mediator.Send(command);
+
             return NoContent();
         }
     }
