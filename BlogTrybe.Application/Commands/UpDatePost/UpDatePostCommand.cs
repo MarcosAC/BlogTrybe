935c5c55783fb56bcceaec7ae2f0b376f42b5a3d
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-05-18 15:17:51 -0300

    Implementação command Update Post

---
+++ BlogTrybe.Application/Commands/UpDatePost/UpDatePostCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BlogTrybe.Application.Commands.UpDatePost
+{
+    public class UpDatePostCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ BlogTrybe.Application/Commands/UpDatePost/UpDatePostCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using BlogTrybe.Core.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlogTrybe.Application.Commands.UpDatePost
+{
+    public class UpDatePostCommandHandler : IRequestHandler<UpDatePostCommand, Unit>
+    {
+        private readonly IPostRepository _postRepository;
+
+        public UpDatePostCommandHandler(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public async Task<Unit> Handle(UpDatePostCommand request, CancellationToken cancellationToken)
+        {
+            var post = await _postRepository.GetByIdAsync(request.Id);
+
+            post.Update(request.Title, request.Content);
+
+            await _postRepository.SaveChangesAsync();
+
+            return Unit.Value;
+        }
+    }
+}
